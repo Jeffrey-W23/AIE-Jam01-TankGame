9745c277adbbb725208374337d870fc75df27e0e
Thomas Wiltshire <thomas.wiltshire14@icloud.com>
2018-03-06 12:26:40 +1100

    First commit.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,175 @@
+﻿// using, etc
+using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+using System.Collections.Generic;
+
+//--------------------------------------------------------------------------------------
+// Enemy object. Inheriting from MonoBehaviour. The main enemy class for enemy behaviour
+//--------------------------------------------------------------------------------------
+public class Enemy : MonoBehaviour
+{
+    // PUBLIC VALUES //
+    //--------------------------------------------------------------------------------------
+    // pulbic int for the max health of the enemy
+    public int m_nMaxHealth;
+
+    // public int for the damage that the enemy can do
+    public int m_nDamage;
+
+    // public foat for the speed of the enemy.
+    public float m_fSpeed;
+
+    // public hover height for enemy
+    public float m_fHoverHeight;
+    //--------------------------------------------------------------------------------------
+
+    // PRIVATE VALUES //
+    //--------------------------------------------------------------------------------------
+    // private NavMeshAgent for the navmesh agent of the enemy
+    private NavMeshAgent m_nmAgent;
+
+    // private transform for the goal postion of the enemy.
+    private Transform m_tGoal;
+
+    // private int for the current health of the enemy
+    private int m_nHealth;
+
+    // private gameobject for the player object
+    private GameObject m_gPlayer;
+    //--------------------------------------------------------------------------------------
+
+    //--------------------------------------------------------------------------------------
+    // initialization.
+    //--------------------------------------------------------------------------------------
+    void Awake()
+    {
+        // Get navmesh agent commponent
+        m_nmAgent = GetComponent<NavMeshAgent>();
+
+        // set the speed of the navmesh agent
+        m_nmAgent.speed = m_fSpeed;
+
+        // set the offset of the navmesh
+        m_nmAgent.baseOffset = m_fHoverHeight;
+
+        // get player object
+        m_gPlayer = GameObject.FindGameObjectWithTag("Player");
+
+        // if valid player
+        if (m_gPlayer)
+        {
+            // set goal to player transform
+            m_tGoal = m_gPlayer.transform;
+        }
+    }
+
+    //--------------------------------------------------------------------------------------
+    // Update: Function that calls each frame to update game objects.
+    //--------------------------------------------------------------------------------------
+    void Update()
+    {
+        // if valid goal
+        if (m_tGoal)
+        {
+            // set navmesh agent destination to goal position.
+            m_nmAgent.destination = m_tGoal.position;
+        }
+        
+        // Debug enemy health value
+        Debug.Log(m_nHealth);
+
+        // Check if the enemy is dead
+        CheckDeath();
+    }
+
+    //--------------------------------------------------------------------------------------
+    // SetHealth: Set the health of the enemy
+    //
+    // Param:
+    //      nHealth: int value for setting health
+    //--------------------------------------------------------------------------------------
+    public void SetHealth(int nHealth)
+    {
+        // set health
+        m_nHealth = nHealth;
+    }
+
+    //--------------------------------------------------------------------------------------
+    // SetDamage: Set the damage of the enemy
+    //
+    // Param:
+    //      nDamage: int value for setting damage
+    //--------------------------------------------------------------------------------------
+    public void SetDamage(int nDamage)
+    {
+        // Set damage
+        m_nDamage = nDamage;
+    }
+
+    //--------------------------------------------------------------------------------------
+    // SetSpeed: Set the speed of the enemy
+    //
+    // Param:
+    //      fSpeed: int value for setting speed
+    //--------------------------------------------------------------------------------------
+    public void SetSpeed(int fSpeed)
+    {
+        // Set damage
+        m_fSpeed = fSpeed;
+
+        // set speed to the navmesh agent
+        m_nmAgent.speed = m_fSpeed;
+    }
+
+    //--------------------------------------------------------------------------------------
+    // CheckDeath: Check if the enemy is dead.
+    //--------------------------------------------------------------------------------------
+    public bool CheckDeath()
+    {
+        // if no health
+        if (m_nHealth <= 0)
+        {
+            // set inactive when dead
+            gameObject.SetActive(false);
+
+            // set health back to full
+            m_nHealth = m_nMaxHealth;
+
+            // return true
+            return true;
+        }
+
+        // else no death then return false
+        return false;
+    }
+
+    //--------------------------------------------------------------------------------------
+    // OnCollisionEnter: Calls when this object has hit a collision.
+    //
+    // Param:
+    //      collision: The object that has been collided with.
+    //--------------------------------------------------------------------------------------
+    private void OnCollisionEnter(Collision collision)
+    {
+        // if the collison is with the player.
+        if (collision.gameObject.tag == "Player")
+        {
+            // set health of the enemy to 0
+            m_nHealth = 0;
+
+            // damage the player
+            //m_gPlayer.mechHealth =- m_nDamage;                                                // UNCOMMENT ONCE MERGED
+        }
+
+        // if the collison is with the player.
+        if (collision.gameObject.tag == "Bullet")
+        {
+            // Get bullet object
+            GameObject gBullet = GameObject.FindGameObjectWithTag("Bullet");
+
+            // set health of the enemy by the bullet damage.
+            //m_nHealth =-gBullet.Damage;                                                       // UNCOMMENT ONCE MERGED
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,162 @@
+﻿// Using, etc
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//--------------------------------------------------------------------------------------
+// SpawnPoint object. Inheriting from MonoBehaviour.Used for spawning the enemies.
+//--------------------------------------------------------------------------------------
+public class SpawnController : MonoBehaviour
+{
+    // PUBLIC VALUES //
+    //--------------------------------------------------------------------------------------
+    // public gameobject for the enemy spawn blueprint
+    public GameObject m_gEnemyBlueprint;
+
+    // public int for array size
+    public int m_nPoolSize;
+
+    // public float for spawn rate
+    public float m_fSpawnRate;
+    //--------------------------------------------------------------------------------------
+
+    // PRVIATE VALUES //
+    //--------------------------------------------------------------------------------------
+    // prviate dynamic array for enemy list
+    private List<GameObject> m_agEnemyList;
+
+    // prviate float for the spawn timer
+    private float m_fSpawnTimer;
+    //--------------------------------------------------------------------------------------
+
+    //--------------------------------------------------------------------------------------
+    // initialization.
+    //--------------------------------------------------------------------------------------
+    void Awake()
+    {
+        // initialize enemylist with size
+        m_agEnemyList = new List<GameObject>();
+
+        // loop through each enemy
+        for (int i = 0; i < m_nPoolSize; ++i)
+        {
+            // Instantiate and set active state.
+            GameObject tmp = Instantiate(m_gEnemyBlueprint);
+            tmp.SetActive(false);
+            m_agEnemyList.Add(tmp);
+        }
+
+        // Set default value for timer
+        m_fSpawnTimer = 0.0f;
+    }
+
+    //--------------------------------------------------------------------------------------
+    // Update: Function that calls each frame to update game objects.
+    //--------------------------------------------------------------------------------------
+    void Update()
+    {
+        // Start the timer // Update by deltatime
+        m_fSpawnTimer += Time.deltaTime;
+
+        // if spawn timer is greater than the spawn rate
+        if (m_fSpawnTimer > m_fSpawnRate)
+        {
+            // reset timer
+            m_fSpawnTimer = 0.0f;
+
+            // Allocate an enemy to the pool
+            GameObject gEnemy = AllocateEnemy();
+
+            // If a valid enemy
+            if (gEnemy)
+            {
+                // put enemy at a random position in the map
+                Vector2 pos = Random.insideUnitCircle.normalized * 500.0f;
+                gEnemy.transform.position = pos;
+            }
+        }
+    }
+
+    //--------------------------------------------------------------------------------------
+    // AllocateEnemy: Allocate enemy to the object pool.
+    //
+    // Return:
+    //      GameObject: Return the allocated gameobject.
+    //--------------------------------------------------------------------------------------
+    GameObject AllocateEnemy()
+    {
+        // for each in the pool
+        for (int i = 0; i < m_nPoolSize; ++i)
+        {
+            // Check if active
+            if (!m_agEnemyList[i].activeInHierarchy)
+            {
+                // Set active state
+                m_agEnemyList[i].SetActive(true);
+
+                // Return the enemy
+                return m_agEnemyList[i];
+            }
+        }
+
+        // If all fail return null;
+        return null;
+    }
+
+    //--------------------------------------------------------------------------------------
+    // AddNewEnemy: Add a new enemy to the object pool.
+    //--------------------------------------------------------------------------------------
+    void AddNewEnemy()
+    {
+        GameObject tmp = Instantiate(m_gEnemyBlueprint);
+        tmp.SetActive(false);
+        m_agEnemyList.Add(tmp);
+    }
+
+    //--------------------------------------------------------------------------------------
+    // SetPoolSize: Set the pool size of the SpawnPoint.
+    //
+    // Param:
+    //      nSize: int for setting pool size
+    //--------------------------------------------------------------------------------------
+    void SetPoolSize(int nSize)
+    {
+        // Set pool size
+        m_nPoolSize = nSize;
+
+        // Add new enemy to the list
+        while (m_nPoolSize < m_agEnemyList.Count) AddNewEnemy();
+    }
+
+    //--------------------------------------------------------------------------------------
+    // SetHealth: Set the health of the enemies in the object pool.
+    //
+    // Param:
+    //      nHealth: int for setting enemy health
+    //--------------------------------------------------------------------------------------
+    void SetHealth(int nHealth)
+    {
+        //; Loop through each enemy
+        for (int i = 0; i < m_nPoolSize; ++i)
+        {
+            // set health
+            m_agEnemyList[i].GetComponent<Enemy>().SetHealth(nHealth);
+        }
+    }
+
+    //--------------------------------------------------------------------------------------
+    // SetDamage: Set the damage of the enemies in the object pool.
+    //
+    // Param:
+    //      nDamage: int for setting enemy damage
+    //--------------------------------------------------------------------------------------
+    void SetDamage(int nDamage)
+    {
+        // Loop through each enemy
+        for (int i = 0; i < m_nPoolSize; ++i)
+        {
+            // set damage
+            m_agEnemyList[i].GetComponent<Enemy>().SetHealth(nDamage);
+        }
+    }
+}

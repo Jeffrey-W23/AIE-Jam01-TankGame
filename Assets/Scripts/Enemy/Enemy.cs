addc0a0591fce73942fc1e00e88b005e2735cea8
Thomas Wiltshire <thomas.wiltshire14@icloud.com>
2018-03-07 11:39:12 +1100

    Merged changes into the main project.

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,236 @@
+﻿//--------------------------------------------------------------------------------------
+// Purpose: Enemy class.
+//
+// Description: The main class for the indivual enemy units.
+//
+// Author: Thomas Wiltshire.
+//--------------------------------------------------------------------------------------
+
+// using, etc
+using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+using System.Collections.Generic;
+
+//--------------------------------------------------------------------------------------
+// Enemy object. Inheriting from MonoBehaviour. The main enemy class for enemy behaviour
+//--------------------------------------------------------------------------------------
+public class Enemy : MonoBehaviour
+{
+    // PUBLIC VALUES //
+    //--------------------------------------------------------------------------------------
+    // pulbic int for the max health of the enemy
+    public int m_nMaxHealth;
+
+    // public int for the damage that the enemy can do
+    public int m_nDamage;
+
+    // public foat for the speed of the enemy.
+    public float m_fSpeed;
+
+    // public hover height for enemy
+    public float m_fHoverHeight;
+
+    // public int for the score of what a enemy is worth.
+    public int m_nScoreValue;
+    //--------------------------------------------------------------------------------------
+
+    // PRIVATE VALUES //
+    //--------------------------------------------------------------------------------------
+    // private NavMeshAgent for the navmesh agent of the enemy
+    private NavMeshAgent m_nmAgent;
+
+    // private transform for the goal postion of the enemy.
+    private Transform m_tGoal;
+    
+    // private int for the current health of the enemy
+    [HideInInspector]                                           // Edited By: Angus
+    public int m_nHealth;
+
+    // private gameobject for the player object
+    private GameObject m_gPlayer;
+
+    // private PlayerStats for the stat class of player.
+    private PlayerStats m_pPlayerStats;
+
+    // the enemy animator
+    private Animator m_aAnimator;
+
+    // boolean for an animation of the enemy death
+    public bool m_bDeathAni;
+    //--------------------------------------------------------------------------------------
+
+    //--------------------------------------------------------------------------------------
+    // initialization.
+    //--------------------------------------------------------------------------------------
+    void Awake()
+    {
+        // Get navmesh agent commponent
+        m_nmAgent = GetComponent<NavMeshAgent>();
+
+        // set the speed of the navmesh agent
+        m_nmAgent.speed = m_fSpeed;
+
+        // set the offset of the navmesh
+        m_nmAgent.baseOffset = m_fHoverHeight;
+
+        // get player object
+        m_gPlayer = GameObject.FindGameObjectWithTag("Player");
+        
+        // get the player stats component
+        m_pPlayerStats = m_gPlayer.GetComponent<PlayerStats>();
+        
+        // if valid player
+        if (m_gPlayer)
+        {
+            // set goal to player transform
+            m_tGoal = m_gPlayer.transform;
+        }
+
+        // Get the component of the animator
+        m_aAnimator = GetComponent<Animator>();
+
+        // set the deafult for the death animation
+        m_bDeathAni = false;
+    }
+
+    //--------------------------------------------------------------------------------------
+    // Update: Function that calls each frame to update game objects.
+    //--------------------------------------------------------------------------------------
+    void Update()
+    {
+        // if valid goal
+        if (m_tGoal)
+        {
+            // set navmesh agent destination to goal position.
+            m_nmAgent.destination = m_tGoal.position;
+        }
+        
+        // Debug enemy health value
+        Debug.Log(m_nHealth);
+
+        // Check if the enemy is dead
+        CheckDeath();
+    }
+
+    //--------------------------------------------------------------------------------------
+    // SetHealth: Set the health of the enemy
+    //
+    // Param:
+    //      nHealth: int value for setting health
+    //--------------------------------------------------------------------------------------
+    public void SetHealth(int nHealth)
+    {
+        // set health
+        m_nHealth += nHealth;
+    }
+
+    //--------------------------------------------------------------------------------------
+    // SetMaxHealth: Set the max health of the enemy
+    //
+    // Param:
+    //      nHealth: int value for setting health
+    //--------------------------------------------------------------------------------------
+    public void SetMaxHealth(int nHealth)
+    {
+        // set health
+        m_nMaxHealth = nHealth;
+    }
+
+    //--------------------------------------------------------------------------------------
+    // SetDamage: Set the damage of the enemy
+    //
+    // Param:
+    //      nDamage: int value for setting damage
+    //--------------------------------------------------------------------------------------
+    public void SetDamage(int nDamage)
+    {
+        // Set damage
+        m_nDamage = nDamage;
+    }
+
+    //--------------------------------------------------------------------------------------
+    // SetSpeed: Set the speed of the enemy
+    //
+    // Param:
+    //      fSpeed: int value for setting speed
+    //--------------------------------------------------------------------------------------
+    public void SetSpeed(int fSpeed)
+    {
+        // Set damage
+        m_fSpeed = fSpeed;
+
+        // set speed to the navmesh agent
+        m_nmAgent.speed = m_fSpeed;
+    }
+
+    //--------------------------------------------------------------------------------------
+    // CheckDeath: Check if the enemy is dead.
+    //--------------------------------------------------------------------------------------
+    public bool CheckDeath()
+    {
+        // if no health
+        if (m_nHealth <= 0)
+        {
+            // play death animation
+            m_bDeathAni = true;
+
+            // set inactive when dead
+            gameObject.SetActive(false);
+            
+            // set health back to full
+            m_nHealth = m_nMaxHealth;
+
+            // reset death animation for next death
+            m_bDeathAni = false;
+
+            // return true
+            return true;
+        }
+
+        // else no death then return false
+        return false;
+    }
+
+    //--------------------------------------------------------------------------------------
+    // OnCollisionEnter: Calls when this object has hit a collision.
+    //
+    // Param:
+    //      collision: The object that has been collided with.
+    //
+    // Edited By:
+    //      Angus
+    //--------------------------------------------------------------------------------------
+    private void OnCollisionEnter(Collision collision)
+    {
+        // if the collison is with the player.
+        if (collision.gameObject.tag == "Player")
+        {
+            // set health of the enemy to 0
+            m_nHealth = 0;
+
+            m_pPlayerStats.health = -m_nDamage;
+
+        }
+
+        // if the collison is with the player.
+        if (collision.gameObject.tag == "Bullet")
+        {
+            Debug.Log("Hit enemy!");
+            // Get bullet object
+            //   GameObject gBullet = GameObject.FindGameObjectWithTag("Bullet");
+
+            //Grab bullet information from object.
+            BulletScript bullet = collision.gameObject.GetComponent<BulletScript>();
+
+            //take bullet damage away from health.
+            m_nHealth =- bullet.bulletDamage;
+
+            if(m_nHealth < 0)
+            {
+                //Add 10 points to score.
+                m_pPlayerStats.score += 10;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,350 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    //Players numbers
+    public int playerNumber = 1;
+
+    //Player mech stats and movement variables.
+    public float movementSpeed = 10.0f;
+    public float runMultiplier = 4.0f;
+    public float rotationSpeed = 5.0f;
+    public float gunRotationSpeed = 4.0f;
+    public float jumpHeight = 400.0f;
+
+    //Deadzone (how far the controller must be moved for movement to work.)
+    private float deadZone = 0.1f;
+
+    //Private floats to protect the public variables and copy input values
+    //from controller.
+    private float forwardAcceleration;
+    private float turnValueX;
+    private float turnValueY;
+    private float moveValue;
+    private float stickInputTurnX;
+    private float stickInputTurnY;
+
+    private float moveValueCopy;
+
+    //Copy of the gamepad.
+    [HideInInspector]
+    public xbox_gamepad gamepad;
+
+
+
+    private Rigidbody mechBody;
+
+    //Instances of the mechs body for manipulation.
+    public GameObject lowerBody;
+    public GameObject upperBody;
+    public GameObject railgun;
+    public GameObject machinegun;
+    public PlayerOverheat playerOverheat;
+
+    public GameObject playerMech;
+    private Animator animator;
+
+    AudioSource mechWalking;
+    AudioSource mechJumping;
+    AudioSource mechRunning;
+
+    //Bools to check if the player i
+    [HideInInspector]
+    public bool isShooting = false, isRunning = false, isJumping = false, overheated = false, anchored = false;
+
+    //checks if the mech is grounded.
+    [HideInInspector]
+    public bool grounded = false;
+
+    public float jumpCoolDown = 2f;
+    private float accelerationOnExit;
+
+    private float jumpBuffer = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+     
+        mechBody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+
+        //Assigns controllers based on player number.
+        switch(playerNumber)
+        {
+            case 1:
+               gamepad = GamePadManager.Instance.GetGamePad(1);
+                break;
+            case 2:
+               gamepad = GamePadManager.Instance.GetGamePad(2);
+                break;
+            default:
+                break;
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Since upperbody is broken into two parts upperbody's position = lower bodys position
+        // + y value
+        upperBody.transform.position = new Vector3(lowerBody.transform.position.x, lowerBody.transform.position.y + 1.5f, lowerBody.transform.position.z);
+
+
+        //If both triggers are down the mech is anchored.
+        if(playerNumber == 1)
+        {
+            //Anchoring is deprecated
+            //if(gamepad.GetTriggerDown_L() && gamepad.GetTriggerDown_R())
+            //{
+            //    anchored = true;
+            //}
+            //else if(!gamepad.GetTriggerDown_R() && !gamepad.GetTriggerDown_L())
+            //{
+            //    anchored = false;
+            //}        
+
+            jumpCoolDown -= Time.deltaTime;
+
+            if (grounded && !playerOverheat.overheated)
+            {
+                moveValue = 0;
+                float translation;
+                translation = jumpHeight * Time.deltaTime;
+                if (gamepad.GetTriggerDown_L() && !gamepad.GetTriggerDown_R() && jumpCoolDown < 0)
+                {
+                    jumpBuffer -= Time.deltaTime;
+
+                    if (jumpBuffer < 0)
+                    {
+                        jumpBuffer = 0.1f;
+                        isJumping = true;
+                        jumpCoolDown = 2f;
+                        mechBody.velocity = new Vector3(0, translation, 0);
+                        mechBody.AddForce(new Vector3(0, translation * 60, 0), ForceMode.Impulse);
+                    }
+                }
+            }
+
+            if (!grounded)
+            {
+                isJumping = false;
+                jumpCoolDown = 2f;
+            }
+
+
+        //    Debug.Log("jump cd" + jumpCoolDown);
+
+        }
+
+
+
+        if (playerNumber == 2)
+        {
+           
+        }
+   
+
+    
+
+        //Resets the values so the player stops if controls are let go.
+        turnValueX = 0;
+        turnValueY = 0;
+        forwardAcceleration = 0;
+       
+        if (!playerOverheat.overheated)
+        {
+            //Gets stick input values and sets them to a variable.
+            stickInputTurnX = gamepad.GetStick_R().X;
+            stickInputTurnY = gamepad.GetStick_R().Y;
+            forwardAcceleration = gamepad.GetStick_L().Y;
+
+
+            //Sets the forward acceleration all the way to 1 if the stick
+            //is moved even a little bit diagonal to stop inconsistent movement.
+            if(forwardAcceleration > 0.1f)
+            {
+                forwardAcceleration = 1;
+            }
+            else if(forwardAcceleration < 0.1f && forwardAcceleration > 0)
+            {
+                forwardAcceleration = 0;
+            }
+
+            //sets the input to -1 if the stick is moved.
+            if (forwardAcceleration < -0.1f)
+            {
+                forwardAcceleration = -1;
+            }
+            else if (forwardAcceleration > -0.1f && forwardAcceleration < 0)
+            {
+                forwardAcceleration = 0;
+            }
+            //---------------------------------------------------------------------------
+
+            //if the stick input is greater then the deadzone apply movement.
+            if (Mathf.Abs(stickInputTurnX) > deadZone)
+            {
+                turnValueX = stickInputTurnX;
+            }
+
+            if (Mathf.Abs(stickInputTurnY) > deadZone)
+            {
+                turnValueY = stickInputTurnY;
+            }
+            if (grounded)
+            {
+                if (Mathf.Abs(forwardAcceleration) > deadZone)
+                {
+                    if (gamepad.GetTriggerDown_R() && !playerOverheat.overheated)
+                    {
+                        moveValue = forwardAcceleration * runMultiplier;
+                        isRunning = true;
+                    }
+                    else if(!gamepad.GetTriggerDown_R())
+                    {
+                        moveValue = forwardAcceleration;
+                        isRunning = false;
+                    }
+
+                }
+            }
+            //----------------------------------------------------------------
+        }
+      
+        Debug.Log("X stick value: " + forwardAcceleration);
+        Debug.Log(grounded);
+    }
+
+    private void FixedUpdate()
+    {
+
+        moveCharacter(playerNumber);
+    }
+
+    static float ClampAngle(float angle, float min, float max)
+    {
+        if (min < 0 && max > 0 && (angle > max || angle < min))
+        {
+            angle -= 360;
+            if (angle > max || angle < min)
+            {
+                if (Mathf.Abs(Mathf.DeltaAngle(angle, min)) < Mathf.Abs(Mathf.DeltaAngle(angle, max))) return min;
+                else return max;
+            }
+        }
+        else if (min > 0 && (angle > max || angle < min))
+        {
+            angle += 360;
+            if (angle > max || angle < min)
+            {
+                if (Mathf.Abs(Mathf.DeltaAngle(angle, min)) < Mathf.Abs(Mathf.DeltaAngle(angle, max))) return min;
+                else return max;
+            }
+        }
+
+        if (angle < min) return min;
+        else if (angle > max) return max;
+        else return angle;
+    }
+
+    void moveCharacter(int a_playerNumber)
+    {
+        switch(playerNumber)
+        {
+            case 1:
+                if (grounded)
+                {
+                    if (forwardAcceleration > 0)
+                    {
+                        lowerBody.transform.Translate(-playerMech.transform.right * moveValue * movementSpeed);
+
+                    }
+                    else if (forwardAcceleration < 0)
+                    {
+                        lowerBody.transform.Translate(-playerMech.transform.right * moveValue * movementSpeed);
+                    }
+
+                    if (turnValueX > 0)
+                    {
+                        lowerBody.transform.Rotate(playerMech.transform.up * turnValueX * rotationSpeed);
+                    }
+                    else if (turnValueX < 0)
+                    {
+                        lowerBody.transform.Rotate(playerMech.transform.up * turnValueX * rotationSpeed);
+                    }
+                }
+                else if(!grounded)
+                {
+                    if (moveValue > 0.1f || moveValue < -0.1f)
+                    {
+                        lowerBody.transform.Translate(-playerMech.transform.forward * accelerationOnExit * movementSpeed);
+                    }
+              
+            
+                       // lowerBody.transform.Translate(-playerMech.transform.forward  * movementSpeed);
+                 
+
+                    if (turnValueX > 0)
+                    {
+                        lowerBody.transform.Rotate(playerMech.transform.up);
+                    }
+                    else if (turnValueX < 0)
+                    {
+                        lowerBody.transform.Rotate(playerMech.transform.up);
+                    }
+                }
+                break;
+            case 2:
+
+                float rotationX = upperBody.transform.rotation.x;
+                ClampAngle(upperBody.transform.eulerAngles.x, -15f, 30f);
+
+                if (turnValueX > 0)
+                {
+                    upperBody.transform.Rotate(playerMech.transform.up * turnValueX * rotationSpeed);
+                }
+                else if(turnValueX < 0)
+                {
+                    upperBody.transform.Rotate(playerMech.transform.up * turnValueX * rotationSpeed);
+                }
+                if(turnValueY > 0)
+                {
+                    railgun.transform.Rotate(0, 0, turnValueY * rotationSpeed);
+                    machinegun.transform.Rotate(0, 0, turnValueY * rotationSpeed);
+                }
+                else if(turnValueY < 0)
+                {
+                    railgun.transform.Rotate(0,0, turnValueY * rotationSpeed);
+                    machinegun.transform.Rotate(0, 0, turnValueY * rotationSpeed);
+                }
+  
+
+
+                break;
+            default:
+                break;
+
+
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Terrain")
+        {
+            grounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.tag == "Terrain")
+        {
+            grounded = false;
+            accelerationOnExit = moveValue;
+        }
+    }
+}
+
